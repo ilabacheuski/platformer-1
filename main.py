0b53964ea1353d39b52d83553f208eaf85b10e6a
Maxim Kisel <max.kisel.2006@gmail.com>
2019-01-22 17:57:16 +0300

    lesson-13

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import pygame
 
-
-WIDTH = 640
-HEIGHT = 400
-FPS = 60
+# Определение констант
+WIDTH = 640 # Ширина окна
+HEIGHT = 480 # Высота окна
+FPS = 60 # Кадры в секунду
 TITLE = "Platformer"
 
+# Определение цветов
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0 ,255 ,0)
+GREEN = (0, 255, 0)
 BACKGROUND_COLOR = (64, 177, 247)
 
-pygame.init()
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
-pygame.display.set_caption(TITLE)
+pygame.init() # Инициализация pygame
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Создаем окно для отрисовки
+pygame.display.set_caption(TITLE) # Устанавливает заголовок окна
 
 running = True
+# Игровой цикл
 while running:
-    allEvents = pygame.event.get()
-    for event in allEvents:
-        if event.tupe == pygame.QUIT:
+    # events()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
-            if event.tupe == pygame.KEYDOWN:
-               
+        if event.type == pygame.KEYDOWN: 
+            ''' 
+                Если (if) тип(type) события (event)
+                равен (==)
+                нажатию клавиши на клавиатуре (pygame.KEYDOWN)
+            '''
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
-    screen.fill(BACKGROUND_COLOR)
 
-    pygame.display.flip()
+    # update()
+    pass
 
-pygame quit()
+    # draw()
+    screen.fill(BACKGROUND_COLOR) # Заполняем экран цветом
+    
+    pygame.display.flip() # Отображаем нарисованое в Double buffer
+
+pygame.quit()
